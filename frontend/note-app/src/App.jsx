38ea8d8d5f7ac50b79aa0160208824da079a7caf
Utkarsh601 <utkarshmishra6102@gmail.com>
2025-02-24 20:16:12 +0530

    router updated

--- frontend/note-app/src/App.jsx
+++ frontend/note-app/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Correct imports
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'; // Correct imports
 import Login from './pages/Login/Login';
 import Home from './pages/Home/Home';
 import SignUp from './pages/SignUp/SignUp';
@@ -8,6 +8,7 @@ const App = () => {
   return (
       <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" />} />
         <Route path="/dashboard" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
